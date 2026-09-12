f8b39670bf5bf3def8ebf4425fe446a437bdff0a
choxbrowser <choxbrowser@gmail.com>
2026-09-11 19:02:03 -0600

    Set up Electron app with main window and IPC

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
+const { app, BrowserWindow, ipcMain, shell } = require("electron");
+const path = require("path");
 
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 650,
+
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+ipcMain.handle("open-external", async (_, url) => {
+  if (!/^https?:\/\//i.test(url)) {
+    throw new Error("Only HTTP and HTTPS URLs are allowed.");
+  }
+
+  await shell.openExternal(url);
+  return true;
+});
